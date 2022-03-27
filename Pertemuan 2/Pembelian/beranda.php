fbb4063fa8410078e09f11366473dd236519fe60
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-03-27 23:09:10 +0700

    create beranda.php

---
+++ Pertemuan 2/Pembelian/beranda.php
@@ -0,0 +1,18 @@
+<body>
+	<div class="row">
+	<div class="col-xl-12 col-lg-12 col-md-12">
+	<div class="card shadow-sm my-0">
+	<div class="card-body p-4">
+	<div class="row">
+
+		<div class="col-lg-9">
+		<p>Welcome To WAHYU STORE</p>
+		<p>Selamat Berbelanja :) </p>
+		</div>
+
+	</div>
+	</div>
+	</div>
+	</div>	
+	</div>
+</body>
